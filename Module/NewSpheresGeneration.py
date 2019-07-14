1c1d556d24ae983f18a9a79285b033af326a383a
weihuajing <34654998+jerryweihuajing@users.noreply.github.com>
2019-07-15 00:33:25 +0800

    2019.7.15 0:30

---
+++ Module/NewSpheresGeneration.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 00:25:15 2019
+
+@author:Wei Huajing
+@company:Nanjing University
+@e-mail:jerryweihuajing@126.com
+
+@title：Module-Generate spheres with displacement(New Version)
+"""
+
+import numpy as np
+
+import sys,os
+
+if os.getcwd() not in sys.path:
+    
+    sys.path.append(os.getcwd())
+
+from Object.o_sphere import sphere
+
+from Module import NewPath as NP
+
+#------------------------------------------------------------------------------
+"""
+Generate sample spheres position
+
+Args:
+    case_path: load path of all input files
+    
+Returns:
+    sample spheres position lists
+"""
+def SamplePos(case_path):
+    
+    #path of sample
+    sample_path=case_path.split('input')[0]+'sample.txt'
+    
+    #all lines of sample
+    sample_lines=open(sample_path,'r').readlines()[1:]
+    
+    #position of sample
+    sample_pos=[]
+    
+    for this_line in sample_lines:
+        
+        this_list=this_line.strip('\n').split('\t')
+        
+        #position of this sphere
+        this_pos=np.array([float(item) for item in this_list[:3]])
+    
+        #collect pos
+        sample_pos.append(this_pos)
+        
+    return sample_pos
+
+#------------------------------------------------------------------------------
+"""
+Generate spheres position in a phase
+
+Args:
+    file_path: load path of file
+    
+Returns:
+    spheres position list in a phase
+"""
+def PhasePos(file_path):
+    
+    #position of spheres in a phase
+    phase_pos=[]
+    
+    #all lines
+    lines=open(file_path,'r').readlines()
+    
+    #correct legnth of each line
+    correct_length=len(lines[0].strip('\n').split(','))
+    
+    #traverse all lines
+    for this_line in lines:
+        
+        this_list=this_line.strip('\n').split(',')
+ 
+        #judge if total length is OK
+        if len(this_list)!=correct_length:
+            
+            continue
+          
+        phase_pos.append(np.array([float(this_str) for this_str in this_list[5:8]]))
+        
+    return phase_pos
+        
+#------------------------------------------------------------------------------    
+"""
+Generate spheres position in different phases in a case
+
+Args:
+    case_path: load path of all input files
+    
+Returns:
+    list which contain spheres position list of differnet phase
+"""
+def AllPhasePos(case_path):
+    
+    #input txt file names
+    file_names=NP.FilePathsThisCase(case_path)
+    
+    return [PhasePos(this_file_name) for this_file_name in file_names]
+      
+#------------------------------------------------------------------------------
+"""
+Generate spheres map in a case with assistance of sample.txt
+
+Args:
+    case_path: load path of all input files
+    
+Returns:
+    A map whose values are spheres lists
+"""
+def GenerateSpheresMapWithSample(case_path):
+    
+    #Generate sample spheres position
+    sample_pos=SamplePos(case_path)
+        
+    #spheres position in different phases in a case
+    all_phase_pos=AllPhasePos(case_path)
+
+    #all input data in this path
+    file_paths=NP.FilePathsThisCase(case_path)
+    
+    #spheres map in all phase
+    map_all_phase_spheres={}
+    
+    #traverse all file names
+    for k in range(len(file_paths)):
+    
+        file_path_this_phase=file_paths[k]
+        
+        #all lines
+        lines_this_phase=open(file_path_this_phase,'r').readlines()
+        
+        #list of sphere objects
+        spheres_this_phase=[]
+        
+        #position of spheres in this phase
+        spheres_pos_this_phase=all_phase_pos[k]
+        
+#        print(len(spheres_pos_this_phase))
+#        print(len(lines_this_phase))
+        
+        for kk in range(len(lines_this_phase)):
+      
+            this_list=lines_this_phase[k].strip('\n').split(',')
+                   
+            #define new sphere object
+            new_sphere=sphere()
+            
+            new_sphere.Id=float(this_list[0])
+            new_sphere.radius=float(this_list[1])
+            new_sphere.color=np.array([float(this_str) for this_str in this_list[2:5]])       
+            new_sphere.position=np.array([float(this_str) for this_str in this_list[5:8]])
+            new_sphere.stress_tensor_3D=np.array([float(this_str) for this_str in this_list[8:]])
+         
+            #calculate displacement
+            new_sphere.displacemnet_3D_cumulative=new_sphere.position-sample_pos[kk]
+            
+            #1st phase 
+            if not k:
+                
+                new_sphere.displacemnet_3D_periodical=new_sphere.displacemnet_3D_cumulative
+               
+            #from index 1
+            if k:
+                
+                new_sphere.displacemnet_3D_periodical=new_sphere.position-spheres_pos_this_phase[kk]
+            
+            #3D tensor length is correct
+            if len(new_sphere.stress_tensor_3D)!=9:
+                
+                continue
+            
+            #judge if there is inf
+            if np.inf in new_sphere.stress_tensor_3D or -np.inf in new_sphere.stress_tensor_3D:
+                     
+    #            print(new_sphere.stress_tensor_3D)
+         
+                continue
+            
+            #judge if there is nan
+            for this_element in new_sphere.stress_tensor_3D:
+            
+                if np.isnan(this_element):
+      
+                    continue
+            
+            new_sphere.Init()
+            
+            spheres_this_phase.append(new_sphere)
+            
+#        print(len(spheres_this_phase))
+        
+        #collect it
+        map_all_phase_spheres[k]=spheres_this_phase
+        
+    return map_all_phase_spheres
--- script.py
+++ script.py
@@ -36,6 +36,7 @@ from Module import ValueBoundary as VB
 from Module import ContentBoundary as CB
 from Module import SpheresBoundary as SB
 from Module import SpheresGeneration as SG
+from Module import NewSpheresGeneration as NSG
 
 from Module import StrainPlot as Strain
 from Module import StressPlot as Stress
@@ -43,6 +44,9 @@ from Module import StressPlot as Stress
 '''
 demand 4:
 draw surface with stress or strain figure
+
+demand 5:
+Calculate strain via displacement
 '''
 
 #data folder path
@@ -51,187 +55,8 @@ case_path=os.getcwd()+'\\Data\\base detachment\\fric=0.3 v=1.0\\input\\base=0.00
 #file_names=FileNamesThisCase(case_path)
 file_paths=NP.FilePathsThisCase(case_path)
 
-#Generate spheres
-#
-#for this_file in file_paths:
-#    
-#    print(len(SG.GenerateSpheresFromFile(this_file)))
-
-#displacement cumulative periodical
- 
-#spheres_start=SG.GenerateSpheresFromFile(file_paths[0])
-#
-#spheres_now=SG.GenerateSpheresFromFile(file_paths[-1])
-
-
-#------------------------------------------------------------------------------
-"""
-Generate sample spheres position
-
-Args:
-    case_path: load path of all input files
-    
-Returns:
-    sample spheres position lists
-"""
-def SamplePos(case_path):
-    
-    #path of sample
-    sample_path=case_path.split('input')[0]+'sample.txt'
-    
-    #all lines of sample
-    sample_lines=open(sample_path,'r').readlines()[1:]
-    
-    #position of sample
-    sample_pos=[]
-    
-    for this_line in sample_lines:
-        
-        this_list=this_line.strip('\n').split('\t')
-        
-        #position of this sphere
-        this_pos=np.array([float(item) for item in this_list[:3]])
-    
-        #collect pos
-        sample_pos.append(this_pos)
-        
-    return sample_pos
-
-#------------------------------------------------------------------------------
-"""
-Generate spheres position in a phase
-
-Args:
-    file_path: load path of file
-    
-Returns:
-    spheres position list in a phase
-"""
-def PhasePos(file_path):
-    
-    #position of spheres in a phase
-    phase_pos=[]
-    
-    #all lines
-    lines=open(file_path,'r').readlines()
-    
-    #correct legnth of each line
-    correct_length=len(lines[0].strip('\n').split(','))
-    
-    #traverse all lines
-    for this_line in lines:
-        
-        this_list=this_line.strip('\n').split(',')
- 
-        #judge if total length is OK
-        if len(this_list)!=correct_length:
-            
-            continue
-          
-        phase_pos.append(np.array([float(this_str) for this_str in this_list[5:8]]))
-        
-    return phase_pos
-        
-#------------------------------------------------------------------------------    
-"""
-Generate spheres position in different phases in a case
-
-Args:
-    case_path: load path of all input files
-    
-Returns:
-    list which contain spheres position list of differnet phase
-"""
-def AllPhasepos(case_path):
-    
-    #input txt file names
-    file_names=NP.FilePathsThisCase(case_path)
-    
-    return [PhasePos(this_file_name) for this_file_name in file_names]
-      
-#------------------------------------------------------------------------------
-"""
-Generate spheres map in a case with assistance of sample.txt
-
-Args:
-    case_path: load path of all input files
-    
-Returns:
-    A map whose values are spheres lists
-"""
-def GenerateSpheresMapWithSample(case_path):
-    
-    #Generate sample spheres position
-    sample_pos=SamplePos(case_path)
-        
-    #spheres position in different phases in a case
-    all_phase_pos=AllPhasepos(case_path)
-
-<<<<<<< HEAD
-    #all input data in this path
-    file_paths=NP.FilePathsThisCase(case_path)
-    
-    print(len(all_phase_pos[0]))
-    print(len(sample_pos))
-    
-    for k in range(len(all_phase_pos[0])):
-        
-        print(all_phase_pos[0][0]-sample_pos[0])
-        
-        
-=======
-#    print(len(all_phase_pos[0]))
-#    print(len(sample_pos))
-    
-    for k in range(len(all_phase_pos[0])):
-        
-#        print(all_phase_pos[0][0]-sample_pos[0])
->>>>>>> 1a9ac52132ad0b2f1d1e2e21291d93709e2913f9
-#        
-    
-GenerateSpheresMapWithSample(case_path)
-#------------------------------------------------------------------------------
-"""
-Generate cumulative displacement of spheres
-
-Args:
-    
-    
-Returns:
-    
-"""
-def SpheresCumulativeDisplacement(spheres_start,spheres_now):
-    
-    return
-
-
-#------------------------------------------------------------------------------
-"""
-Generate cumulative strain of spheres
-
-Args:
-    spheres_start: original spheres (from file_names[0])
-    spheres_now: spheres to be processed (from file_names[k])
-    
-Returns:
-    spheres list with cumulative strain
-"""
-def SpheresCumulativeStrain(spheres_start,spheres_now):
-    
-    return
-"""
-Generate periodical strain of spheres
-
-Args:
-    spheres_last: spheres in last period (from file_names[k-1])
-    spheres_now: spheres to be processed (from file_names[k])
-    
-Returns:
-    spheres list with cumulative strain
-"""
-def SpheresPeriodicalStrain(spheres_last,spheres_now):
-    
-    return
+#Generate map between phase index between spheres list 
+MAP=NSG.GenerateSpheresMapWithSample(case_path)
 
 #print(folder_path)
 

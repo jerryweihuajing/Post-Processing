e0ac56a31be710cbb751bf01363371fcb9b41cb6
weihuajing <jerryweihuajing@126.com>
2019-12-01 17:11:10 +0800

    surface init

--- 2D/script.py
+++ 2D/script.py
@@ -54,16 +54,19 @@ a_progress=PO.ProgressConstruction(structural_deformation_path)
 
 #CO.CasePostProcessing(case_path,output_folder)
 
-import cv2
-
 import Image as Img
 
 img_rgb=Img.ImgFlip(a_progress.structural_deformation,0)
 img_tag=Img.ImgFlip(a_progress.img_tag,0)
 
+import numpy as np
 import matplotlib.pyplot as plt
 
-laplacian=cv2.Laplacian(img_tag,cv2.CV_64F)
+#import cv2
+#laplacian=cv2.Laplacian(img_tag,cv2.CV_64F)
+#
+#gradient_x=np.gradient(img_tag)[0]
+#gradient_y=np.gradient(img_tag)[1]
 
 #sobel=cv2.Sobel(img_tag,cv2.CV_64F,1,1,ksize=3)
 #sobelx=cv2.Sobel(img_tag,cv2.CV_64F,1,0,ksize=3)
@@ -74,11 +77,50 @@ laplacian=cv2.Laplacian(img_tag,cv2.CV_64F)
 #plt.subplot(222),plt.imshow(sobel)
 #plt.subplot(223),plt.imshow(sobelx)
 #plt.subplot(224),plt.imshow(sobelx)
+#
+#'''ok!'''
+#plt.figure()
+#plt.subplot(211),plt.imshow(img_rgb)
+#plt.subplot(212),plt.imshow(laplacian)
+#
+#
+#plt.figure()
+#plt.subplot(211),plt.imshow(gradient_x)
+#plt.subplot(212),plt.imshow(gradient_y)
+#
+#np.where(laplacian!=0)
 
-'''ok!'''
+def SimpleGradient(which_matrix,axis=0):
+    
+    simple_gradient=np.zeros(np.shape(which_matrix))
+    
+    if axis==0:
+        
+        simple_gradient[1:,:]=which_matrix[1:,:]-which_matrix[:-1,:]
+        
+    if axis==1:
+        
+        simple_gradient[:,1:]=which_matrix[:,1:]-which_matrix[:,:-1]
+        
+    return simple_gradient
+
+manual_gradient_x=SimpleGradient(img_tag,0)
+manual_gradient_y=SimpleGradient(img_tag,1)
+
+#plt.figure()
+#plt.subplot(211),plt.imshow(manual_gradient_x)
+#plt.subplot(212),plt.imshow(manual_gradient_y)
+
+manual_gradient=manual_gradient_x+manual_gradient_y
+
+surface=np.full(np.shape(manual_gradient),np.nan)
+
+surface[np.where(manual_gradient!=0)]=1
+
 plt.figure()
-plt.subplot(211),plt.imshow(img_rgb)
-plt.subplot(212),plt.imshow(laplacian)
+plt.imshow(img_rgb)
+plt.imshow(surface,cmap='gray')
 
+#RGB channel
 
-
+#surface outline show some similarity

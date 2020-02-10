ec7c6b9a4f489e6dbb941b173048ef847b1a18f6
weihuajing <jerryweihuajing@126.com>
2020-02-11 01:55:55 +0800

    gn KAMERAWEK

--- 2D/script.py
+++ 2D/script.py
@@ -19,7 +19,7 @@ demand 5:
 
 from __init__ import *
 
-case_path=r'D:\GitHub\YADE\Controlling-Simulation\2D\compression 100-500\input\single'
+case_path=r'H:\GitHub\YADE\Controlling-Simulation\2D\compression 100-500\input\single'
 
 #step 1
 C_C_E.CaseCalculation(case_path,which_mode_list='standard')

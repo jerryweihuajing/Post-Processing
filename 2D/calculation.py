ace4bf9a36a118591c486dafdcfe7fe25fd865cf
weihuajing <jerryweihuajing@126.com>
2020-02-13 23:20:27 +0800

    Update calculation.py

--- 2D/calculation.py
+++ 2D/calculation.py
@@ -11,14 +11,14 @@ Created on Thu Feb 13 21:23:14 2020
 
 from __init__ import *
 
-folder_input=r'H:\GitHub\YADE\Controlling-Simulation\2D\compression 100-500\output'
+folder_input=r'H:\GitHub\YADE\Controlling-Simulation\2D\compression 100-500\input'
 folder_output=folder_input.replace('input','output')
 
-list_case_input=[folder_input+'\\'+this_case_name for this_case_name in os.listdir(folder_input)]
-list_case_output=[folder_output+'\\'+this_case_name for this_case_name in os.listdir(folder_output)]
+list_case_input=[this_case_name for this_case_name in os.listdir(folder_input)]
+list_case_output=[this_case_name for this_case_name in os.listdir(folder_output)]
 
-for this_case_path in list_case_input:
+for this_case_name in list_case_input:
     
-    if this_case_path not in list_case_output:
+    if this_case_name not in list_case_output:
         
-        C_C_E.CaseCalculation(this_case_path,which_mode_list='standard')
+        C_C_E.CaseCalculation(folder_input+'\\'+this_case_name,which_mode_list='standard')

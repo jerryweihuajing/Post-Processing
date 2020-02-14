3ff98f746f8e7bd9b138bd2fc7c83c3132baee90
jerryweihuajing <jerryweihuajing@126.com>
2020-02-14 23:20:51 +0800

    Update visualization.py

--- 2D/visualization.py
+++ 2D/visualization.py
@@ -27,7 +27,10 @@ for this_case_name in list_case_output:
         if this_case_name not in list_case_Figures:
             
             progress_folder=folder_output+'\\'+this_case_name+'\\structural deformation\\values'
-            progress_name=os.listdir(progress_folder)[-1]
+            
+            #looking for final progress by index of max
+            list_percentage=[float(this_str.split('%')[0]) for this_str in os.listdir(progress_folder)]
+            progress_name=os.listdir(progress_folder)[list_percentage.index(max(list_percentage))]
             
             final_progress=V_P.ProgressConstruction(progress_folder+'\\'+progress_name)
             

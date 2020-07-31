0ebdc2faf287813e4764ea6304f1157754bdaf50
jerryweihuajing <jerryweihuajing@126.com>
2020-07-31 18:31:39 +0800

    oj

---
+++ cpp/2D/main.cpp
@@ -0,0 +1,18 @@
+﻿// -*- coding: utf-8 -*-
+/******************************************************************************
+@author: Wei Huajing
+@company: Nanjing University
+@e-mail: jerryweihuajing@126.com
+
+@title: Script-main
+******************************************************************************/
+
+// main.cpp: This file contains the "main" function.
+// This is where program execution begins and ends
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
